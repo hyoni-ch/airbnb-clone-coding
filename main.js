f0fa1e75c485372579be15cb147f9caa2191790f
hahyeon <hh_ch0@naver.com>
2024-08-06 11:14:21 +0900

    airbnb clone coding

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelectorAll(".category-button").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    document.querySelector("#active-category-button").removeAttribute("id");
+
+    button.setAttribute("id", "active-category-button");
+  });
+});
